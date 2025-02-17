7c18c938ef93ee39c1bdb551bacea82e54c40040
ElenaKovaleva <lenshemeta@gmail.com>
2025-02-17 19:54:33 +0300

    added log4j2

---
+++ logging/src/main/java/ru/aston/kovaleva/MyClass.java
@@ -0,0 +1,15 @@
+package ru.aston.kovaleva;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MyClass {
+
+    private static final Logger logger = LogManager.getLogger(MyClass.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Run");
+
+    }
+}
